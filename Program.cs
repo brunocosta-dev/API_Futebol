758595733b70541da677003014c30aeca8d31ac5
Bruno Costa <bruno.costa.dev@outlook.com>
2025-12-14 12:04:00 -0300

    Finalizacao dos metodos PUT e DELETE

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using FutebolApi;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 
@@ -17,6 +18,39 @@ app.MapGet("/times/{id}", async (int id, AppDbContext db)=>
 {
     var time = await db.Times.FindAsync(id);
     return time is not null ? Results.Ok(time): Results.NotFound("Time não encontrado");
+});
+
+app.MapPost("/times", async (AppDbContext db, [FromBody] Time novoTime) =>
+{
+    db.Times.Add(novoTime);
+    await db.SaveChangesAsync();
+    return Results.Created($"O time {novoTime.Nome} foi adicionado com sucesso!", novoTime);
+});
+
+app.MapPut("/times/{id}", async (int id, AppDbContext db, [FromBody] Time timeAtualizado) =>
+{
+    var time = await db.Times.FindAsync(id);
+    if(time is null) return Results.NotFound("Time não encontrado");
+
+    time.Nome = timeAtualizado.Nome;
+    time.Cidade = timeAtualizado.Cidade;
+    time.TitulosBrasileiros = timeAtualizado.TitulosBrasileiros;
+    time.TitulosMundiais = timeAtualizado.TitulosMundiais;
+
+   await db.SaveChangesAsync();
+   return Results.Ok(time);
+});
+
+app.MapDelete("/times/{id}", async (int id, AppDbContext db) =>
+{
+    var time = await db.Times.FindAsync(id);
+    if(time is null) return Results.NotFound("Time não encontrado");
+
+    db.Remove(time);
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
 });
 
 app.Run();
