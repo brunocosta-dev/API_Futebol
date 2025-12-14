bd486ba82004e21b39273cd6f08c91a1eb630f17
Bruno Costa <bruno.costa.dev@outlook.com>
2025-12-14 16:18:45 -0300

    Conexao do front com o back-end

--- front-end/script.js
+++ front-end/script.js
@@ -1,8 +1,31 @@
-const fetchAPI = fetch('http://localhost:5020/times', {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json'
-    }
-}).then((res) => res.json()).then((data) => console.log(data));
+const listaTimes = document.getElementById('times-lista');
+const apiUrl = 'http://localhost:5020/times';
 
-console.log(fetchAPI);
+const getTimes = async () => {
+    try{
+        const response = await fetch(apiUrl,{
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if(!response.ok){
+            throw new Error('Erro ao buscar os times!');
+        }
+
+        const times = await response.json();
+        
+        times.forEach((time) => {
+            const newLi = document.createElement('li');
+            newLi.innerText = `Nome: ${time.nome}`;
+            listaTimes.appendChild(newLi);
+        });
+        
+    }
+    catch(error){
+        listaTimes.innerText = `${error.message}`;
+    }
+}
+
+getTimes();
